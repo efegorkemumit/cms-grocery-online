1f8e12a75270604fd978a503ce7d09668f3a39f7
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-04-17 17:32:36 +0300

    Checkout  UI and collection

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,25 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface OrderItemListOrderItemList extends Schema.Component {
+  collectionName: 'components_order_item_list_order_item_lists';
+  info: {
+    displayName: 'OrderItemList';
+  };
+  attributes: {
+    quantity: Attribute.Integer;
+    price: Attribute.Decimal;
+    product: Attribute.Relation<
+      'order-item-list.order-item-list',
+      'oneToOne',
+      'api::product.product'
+    >;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'order-item-list.order-item-list': OrderItemListOrderItemList;
+    }
+  }
 }
